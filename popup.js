9cec892dfa67b7ef8239a6284758380dcfc85331
Alireza <alireza.mh@gmail.com>
2018-02-02 13:25:12 +0330

    Add btch submit

--- popup.js
+++ popup.js
@@ -1,2 +1,24 @@
-document.getElementsByTagName("body")[0].style.color = "green";
-console.log("come over here");
+console.log("10");
+document.addEventListener('DOMContentLoaded', function () {
+    document.querySelector('button').addEventListener('click', submitToTwitter);
+});
+
+function submitToTwitter() {
+    var batchText = document.getElementById("batch_mutter").value;
+    window.close();
+    if(batchText === "") return;
+    var item = JSON.stringify(batchText);
+    var script = "localStorage.setItem('twitter_mutter' ," + item +  ");" ;
+    chrome.tabs.executeScript({
+        code: script
+    });
+
+    chrome.tabs.executeScript( null , {
+        file: "script.js"
+    });
+    chrome.storage.sync.set(batchText);
+    var btn = document.querySelector(".MutedKeywordsAddItemForm-button");
+}
+
+
+
--- script.js
+++ script.js
@@ -1,3 +1,25 @@
-console.log("i am here");
-var ali = document.getElementsByClassName("MutedKeywordsActions-openDialogButton");
-console.log("ali", ali);
+i = 0;
+function setTwitter() {
+    loopOfSubmit();
+}
+setTwitter();
+
+function loopOfSubmit() {
+    let rawData = (localStorage.getItem("twitter_mutter"));
+    let splitData = rawData.split(",");
+    setTimeout(() => {
+        document.querySelector(".MutedKeywordsActions-openDialogButton").click();
+        let input = document.querySelector(".MutedKeywordsAddItemForm-input > .rich-editor");
+        input.innerHTML = "<div>" + splitData[i].toString() + "<div>";
+        // var event = new Event('change');
+        // input.dispatchEvent(event);
+        let Btn = document.querySelector(".MutedKeywordsAddItemForm-button");
+        setTimeout(() => {
+            Btn.click();
+        }, 500);
+        i++;
+        if (i < splitData.length) {
+            loopOfSubmit();
+        }
+    }, 2000);
+}
