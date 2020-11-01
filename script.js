8f4607b2be6c6a9a106f1b54ae667e2b0fcb061d
Alireza <alireza.mh@gmail.com>
2020-11-01 15:28:22 +0330

    fix: update to work with latest UI

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let i = 0;
+var i = 0;
 function setTwitter() {
     loopOfSubmit();
 }
@@ -13,16 +13,36 @@ function loopOfSubmit() {
         splitData = rawData.split("\n");
     }
     setTimeout(() => {
-        document.querySelector(".MutedKeywordsActions-openDialogButton").click();
-        let input = document.querySelector(".MutedKeywordsAddItemForm-input > .rich-editor");
-        input.innerHTML = "<div>" + splitData[i].toString() + "<div>";
-        let Btn = document.querySelector(".MutedKeywordsAddItemForm-button");
+        let inputTwitrer = document.querySelector("input[name='keyword']");
+        dispatchEventValue(inputTwitrer, splitData[i].toString());
+        let Btn = document.querySelector("div[data-testid='settingsDetailSave']");
         setTimeout(() => {
             Btn.click();
         }, 500);
         i++;
+        setTimeout(() => {
+            document.querySelector("a[aria-label*='muted']").click();
+        }, 1500);
         if (i < splitData.length) {
             loopOfSubmit();
         }
     }, 2000);
+}
+
+/**
+ * @method dispatchEventValue
+ * @description dispatch event for react component
+ * @param targetInput
+ * @param value
+ */
+function dispatchEventValue(targetInput, value) {
+    let input = targetInput;
+    let lastValue = input.value;
+    input.value = value;
+    let event = new Event('input', { bubbles: true });
+    let tracker = input._valueTracker;
+    if (tracker) {
+        tracker.setValue(lastValue);
+    }
+    input.dispatchEvent(event);
 }
