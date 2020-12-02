df625da57e8e243bcd8d4c8cf4a8612dcc5c7328
greg <Alireza.mh@gmail.com>
2020-12-02 18:25:15 +0330

    fix: better support

--- popup.js
+++ popup.js
@@ -2,9 +2,9 @@
  * @desc Wait until all DOM loaded then execute DOM manipulation
  */
 document.addEventListener('DOMContentLoaded', function () {
-    const btnMutter  = document.querySelector('#btn_mutter');
-    const linkMore  = document.querySelector('#link-more');
-    const btnMore  = document.querySelector('#btn-more');
+    var btnMutter  = document.querySelector('#btn_mutter');
+    var linkMore  = document.querySelector('#link-more');
+    var btnMore  = document.querySelector('#btn-more');
     btnMutter && btnMutter.addEventListener('click', submitToTwitter);
     linkMore && linkMore.addEventListener("click" , function () {
         this.style.display = "none" ;
@@ -52,7 +52,7 @@ function categoryMutter() {
     if(document.querySelector("input[name=curse]").checked && !already["curse"]){
         loadInput();
         already["curse"] = true;
-        let curseUrl = "https://raw.githubusercontent.com/farzadme/MuteWords/master/Curse%20-%20%D8%A7%D9%84%D9%81%D8%A7%D8%B8%20%D9%86%D8%A7%D9%85%D9%86%D8%A7%D8%B3%D8%A8%20%D9%88%20%D8%B1%DA%A9%DB%8C%DA%A9.txt";
+        var curseUrl = "https://raw.githubusercontent.com/farzadme/MuteWords/master/Curse%20-%20%D8%A7%D9%84%D9%81%D8%A7%D8%B8%20%D9%86%D8%A7%D9%85%D9%86%D8%A7%D8%B3%D8%A8%20%D9%88%20%D8%B1%DA%A9%DB%8C%DA%A9.txt";
         fetch(curseUrl).then(function(response) {
             response.text().then(function(text) {
                 document.getElementById("batch_mutter").value += (document.getElementById("batch_mutter").value) ? "\n" + text : text;
@@ -65,7 +65,7 @@ function categoryMutter() {
     if(document.querySelector("input[name=movie]").checked && !already["movie"]){
         loadInput();
         already["movie"] = true;
-        let movieUrl = "https://raw.githubusercontent.com/farzadme/MuteWords/master/Movie%20%26%20Series%20-%20%D9%81%DB%8C%D9%84%D9%85%20%D9%88%20%D8%B3%D8%B1%DB%8C%D8%A7%D9%84.txt";
+        var movieUrl = "https://raw.githubusercontent.com/farzadme/MuteWords/master/Movie%20%26%20Series%20-%20%D9%81%DB%8C%D9%84%D9%85%20%D9%88%20%D8%B3%D8%B1%DB%8C%D8%A7%D9%84.txt";
         fetch(movieUrl).then(function(response) {
             response.text().then(function(text) {
                 document.getElementById("batch_mutter").value += (document.getElementById("batch_mutter").value) ? "\n" + text : text;
@@ -78,7 +78,7 @@ function categoryMutter() {
     if(document.querySelector("input[name=sport]").checked && !already["sport"]){
         loadInput();
         already["sport"] = true;
-        let sportUrl = "https://raw.githubusercontent.com/farzadme/MuteWords/master/Football%20%26%20Sports%20-%20%D9%81%D9%88%D8%AA%D8%A8%D8%A7%D9%84%20%D9%88%20%D9%88%D8%B1%D8%B2%D8%B4%DB%8C.txt";
+        var sportUrl = "https://raw.githubusercontent.com/farzadme/MuteWords/master/Football%20%26%20Sports%20-%20%D9%81%D9%88%D8%AA%D8%A8%D8%A7%D9%84%20%D9%88%20%D9%88%D8%B1%D8%B2%D8%B4%DB%8C.txt";
         fetch(sportUrl).then(function(response) {
             response.text().then(function(text) {
                 document.getElementById("batch_mutter").value += (document.getElementById("batch_mutter").value) ? "\n" + text : text;
@@ -91,7 +91,7 @@ function categoryMutter() {
     if(document.querySelector("input[name=political]").checked && !already["political"]){
         loadInput();
         already["political"] = true;
-        let politicalUrl = "https://raw.githubusercontent.com/farzadme/MuteWords/master/Political%20-%20%D8%B3%DB%8C%D8%A7%D8%B3%DB%8C.txt";
+        var politicalUrl = "https://raw.githubusercontent.com/farzadme/MuteWords/master/Political%20-%20%D8%B3%DB%8C%D8%A7%D8%B3%DB%8C.txt";
         fetch(politicalUrl).then(function(response) {
             response.text().then(function(text) {
                 document.getElementById("batch_mutter").value += (document.getElementById("batch_mutter").value) ? "\n" + text : text;
@@ -102,7 +102,7 @@ function categoryMutter() {
         });
     }
     if(document.querySelector("input[name=custom]").value !== ""){
-        let url = document.querySelector("input[name=custom]").value;
+        var url = document.querySelector("input[name=custom]").value;
         fetch(url).then(function(response) {
             response.text().then(function(text) {
                 document.getElementById("batch_mutter").value += (document.getElementById("batch_mutter").value) ? "\n" + text : text;
